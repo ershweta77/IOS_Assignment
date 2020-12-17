ac35c2af6f8203cc6980a655151f2acaa79edeac
edusysshweta <edusysshweta@gmail.com>
2020-12-18 00:26:15 +0530

    Books list controller
    added change date button

---
+++ BooksApp/BooksApp/BooksListController.swift
@@ -0,0 +1,25 @@
+//
+//  BooksListController.swift
+//  BooksApp
+//
+//  Created by Apple on 17/12/20.
+//  Copyright © 2020 Assignment. All rights reserved.
+//
+
+import UIKit
+
+class BooksListController: UITableViewController {
+
+    @IBOutlet weak var changeDateBtn: UIBarButtonItem!
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func changeDate(_ sender: UIBarButtonItem) {
+       }
+    
+
+
+}
+
